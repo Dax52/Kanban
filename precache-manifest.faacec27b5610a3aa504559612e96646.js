f7420995da643b1b656882c16b0397fece6edde4
Dax <68510253+Dax52@users.noreply.github.com>
2020-09-21 19:02:07 +0300

    Updates

---
+++ precache-manifest.faacec27b5610a3aa504559612e96646.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d96018bb55b09d4b7553f0d49b111f",
+    "url": "/Kanban/index.html"
+  },
+  {
+    "revision": "7572ea014cc03493c240",
+    "url": "/Kanban/static/css/main.43a9d76d.chunk.css"
+  },
+  {
+    "revision": "3e04fca2537e84858872",
+    "url": "/Kanban/static/js/2.2938adf3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Kanban/static/js/2.2938adf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7572ea014cc03493c240",
+    "url": "/Kanban/static/js/main.6e1042f9.chunk.js"
+  },
+  {
+    "revision": "a073fc2d1e33dd3c3d7a",
+    "url": "/Kanban/static/js/runtime-main.342bd006.js"
+  },
+  {
+    "revision": "4f04ffa0e94d68b68639d5ad2e197cd1",
+    "url": "/Kanban/static/media/Group3.4f04ffa0.svg"
+  },
+  {
+    "revision": "fe1990db4440fe0721a9418d4083910b",
+    "url": "/Kanban/static/media/arrow-down.fe1990db.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Kanban/static/media/logo.5d5d9eef.svg"
+  }
+]);
